d1c875c959a108c0620f6b5da7c7be0ed376ce38
超 <mdc12300@163.com>
2021-09-22 14:59:51 +0800

    增加hello接口

--- src/main/java/com/plat/auto/test/autotest/AutoTestApplication.java
+++ src/main/java/com/plat/auto/test/autotest/AutoTestApplication.java
@@ -2,6 +2,8 @@ package com.plat.auto.test.autotest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AutoTestApplication {
@@ -9,5 +11,15 @@ public class AutoTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutoTestApplication.class, args);
 	}
+
+}
+@RestController
+class testController{
+
+	@GetMapping("/hello")
+	public String hello() throws Exception {
+		return "大聪明啊大聪明";
+	}
+
 
 }
