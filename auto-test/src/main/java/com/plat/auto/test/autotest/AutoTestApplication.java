e5060dfe3ad98daf2e2ef3aa3e939a67a5565647
丑牛 <961986003@qq.com>
2021-09-22 10:46:19 +0800

    首次提交

---
+++ auto-test/src/main/java/com/plat/auto/test/autotest/AutoTestApplication.java
@@ -0,0 +1,13 @@
+package com.plat.auto.test.autotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestApplication.class, args);
+	}
+
+}
---
+++ auto-test/src/test/java/com/plat/auto/test/autotest/AutoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plat.auto.test.autotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
